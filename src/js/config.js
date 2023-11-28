360afbad175b92b0ca9bae5864c1f3cc474fcea5
tuinvurk <tuinvurksteve@gmail.com>
2023-11-28 20:12:10 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f3311ae1-c2e8-43cc-9248-c82a4c7b12fb';
+export const MODAL_CLOSE_SEC = 2.5;
