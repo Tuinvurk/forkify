c9669aee4478194ef064e4b5f3b48b22b7149f95
tuinvurk <tuinvurksteve@gmail.com>
2023-11-28 21:05:47 +0200

    removed some unwanted code

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,11 +119,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the app');
-  console.log('Welcome to the app 2');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,6 +127,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
